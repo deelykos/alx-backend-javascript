c9360e4c45b05a43f3f139d2d5061d34fa96f071
deelykos <fabianohaz@gmail.com>
2023-12-06 18:21:37 +0100

    then,catch,finally

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
+}
+export default handleResponseFromAPI;
