ecf5cb47f04e546b498aab04622a0065b7fb143a
deelykos <fabianohaz@gmail.com>
2023-12-06 22:41:06 +0100

    Handled multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const result = [];
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => {
-      results.map((status, value, reason) => ({
-        status,
-        value: status === 'fulfilled' ? value : reason.toString(),
-      }));
+      results.map(({ status, value, reason }) => (
+        result.push({
+          status,
+          value: status === 'fulfilled' ? value : reason.toString(),
+        })
+      ));
+      return result;
     });
 }
