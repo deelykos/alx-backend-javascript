06c4b07072b1be49e2a4084c325892ee2e0d952e
deelykos <fabianohaz@gmail.com>
2023-12-07 00:29:30 +0100

    await is the new then()

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import { uploadPhoto, createUser } from './utils';
+
+const asyncUploadUser = async () => {
+  let result = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    result = { photo, user };
+  } catch (err) {
+    result = { photo: null, user: null };
+  }
+  return result;
+};
+
+export default asyncUploadUser;
