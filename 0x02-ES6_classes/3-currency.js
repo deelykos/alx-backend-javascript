c25cc8d9c07e244663bce249e0ff059b3e5f11c4
deelykos <fabianohaz@gmail.com>
2023-12-11 22:32:47 +0100

    object literals

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -2,39 +2,38 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-underscore-dangle */
 class Currency {
-    constructor(code, name) {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
       this._code = code;
-      this._name = name;
-    }
-  
-    get code() {
-      return this._code;
-    }
-  
-    set code(code) {
-      if (typeof code === 'string') {
-        this._code = code;
-      } else {
-        throw new TypeError('Code  must be a string');
-      }
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set name(name) {
-      if (typeof name === 'string') {
-        this._name = name;
-      } else {
-        throw new TypeError('Name  must be a string');
-      }
-    }
-  
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
+    } else {
+      throw new TypeError('Code  must be a string');
     }
   }
-  
-  export default Currency;
-  
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name  must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
