dbfa60c7fa5f200df42df8394c44d39782cf14e9
deelykos <fabianohaz@gmail.com>
2023-12-11 10:21:41 +0100

    Created class with new

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-unused-vars */
+import ClassRoom from './0-classroom';
+
+const initializeRooms = () => [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+
+export default initializeRooms;
