c58cd8e75e7fed224776cefd23349152228b8022
deelykos <fabianohaz@gmail.com>
2023-12-12 03:30:24 +0100

    ccorrection

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -6,6 +6,30 @@ class Car {
     this._color = color;
   }
 
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
   static get [Symbol.species]() {
     return this;
   }
