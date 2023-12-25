965bbd221d80f330c092e6f0f3a5569d0b5bd313
deelykos <fabianohaz@gmail.com>
2023-12-25 14:41:39 +0100

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,7 @@
 const cleanSet = (set, startString) => {
   const resultArray = [];
 
-  if (typeof set !== 'object' || !startString || !(set instanceof Set) || typeof startString !== 'string') {
-    return '';
-  }
+  if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) return '';
 
   for (const elem of set) {
     if (elem.startsWith(startString)) {
