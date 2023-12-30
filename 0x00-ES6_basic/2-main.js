acabc5c01430afc66fdd9462aab0715f3b56dc1c
deelykos <fabianohaz@gmail.com>
2023-12-30 08:24:25 +0100

    deleted directory

--- 0x00-ES6_basic/2-main.js
+++
@@ -1,5 +0,0 @@
-import getNeighborhoodsList from './2-arrow';
-
-const neighborhoodsList = new getNeighborhoodsList(); // eslint-disable-line new-cap
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
